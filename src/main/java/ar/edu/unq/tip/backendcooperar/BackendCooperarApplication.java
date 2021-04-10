ab3f71ca90eb59e148581dc2910e7aa7f170722d
Brian Goldman <leonel89011@gmail.com>
2021-04-10 19:42:32 -0300

    Project creation with Spring Initializr

---
+++ src/main/java/ar/edu/unq/tip/backendcooperar/BackendCooperarApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.tip.backendcooperar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCooperarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCooperarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unq/tip/backendcooperar/BackendCooperarApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.tip.backendcooperar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCooperarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
